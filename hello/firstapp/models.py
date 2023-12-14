b76b3917f5fd3409232999d564fe6a528f63d52e
BakSnn <baskarev.nikita@mail.ru>
2023-12-14 21:55:05 +0300

    the addition of a product photo has been implemented

--- hello/firstapp/models.py
+++ hello/firstapp/models.py
@@ -3,9 +3,10 @@ from django.db import models
 class Person(models.Model):
 
     name = models.CharField(max_length=20)
-    age = models.TextField()
+    age = models.DateField()
     maker = models.TextField(max_length=20, default='')
     price = models.FloatField(max_length=10, default='')
+    photo = models.ImageField(upload_to='photos/', blank=True, null=True)
     category = models.CharField(max_length=20)
     objects = models.Manager()
 
--- hello/firstapp/views.py
+++ hello/firstapp/views.py
@@ -4,9 +4,16 @@ from .models import Person
 
 # Получение данных из БД и загрузка index.html
 
+# views.py
+from django.shortcuts import render
+from django.http import HttpResponseRedirect, HttpResponseNotFound
+from .models import Person
+
+# Получение данных из БД и загрузка index.html
 def index(request):
     people = Person.objects.all()
-    return render(request, "index.html", {"people":people})
+    return render(request, "index.html", {"people": people})
+
 # Сохранение данных в БД
 def create(request):
     if request.method == "POST":
@@ -15,24 +22,29 @@ def create(request):
         klient.age = request.POST.get("age")
         klient.maker = request.POST.get("maker")
         klient.price = request.POST.get("price")
+        klient.photo = request.FILES.get("photo")
         klient.category = request.POST.get("category")
         klient.save()
     return HttpResponseRedirect("/")
+
 # Изменение данных в БД
-def edit(request,id):
+def edit(request, id):
     try:
-        person = Person.objects.get(id = id)
+        person = Person.objects.get(id=id)
         if request.method == "POST":
             person.name = request.POST.get('name')
             person.age = request.POST.get('age')
             person.maker = request.POST.get('maker')
             person.price = request.POST.get('price')
             person.category = request.POST.get("category")
+            person.photo = request.FILES.get("photo")
             person.save()
             return HttpResponseRedirect("/")
-        else: return render(request, 'edit.html')
+        else:
+            return render(request, 'edit.html')
     except Person.DoesNotExist:
         return HttpResponseNotFound("<h2>Товар не найден</h2>")
+
 
 def delete(request,id):
     try:
